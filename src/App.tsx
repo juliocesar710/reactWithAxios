c7251a2f20a82eb3b66b8bda39f4cedce2a5fb0f
Júlio César <julio.simao@aluno.uepb.edu.br>
2024-04-30 01:48:34 +0000

    ativiade feita

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,17 @@ import { useCallback, useEffect, useState } from 'react';
 import './App.css'
 import axios from 'axios';
 
-interface Advice{
-  id: number;
-  advice: string;
-}
-
 function App() {
 
-  const [nome, setNome] = useState<String>();
-  const [advice, setAdvice] = useState<String>();
+  const [nome, setNome] = useState<string>(); 
+  const [characterName, setCharacterName] = useState<string>(); 
 
   const getData = useCallback( async()=> {
-    await axios.get('https://api.adviceslip.com/advice')
+  
+    await axios.get('https://swapi.py4e.com/api/people/3/')
     .then(function (response) {
-     console.log(response.data);
-     setAdvice(response.data.slip.advice);
+    console.log(response.data);
+    setCharacterName(response.data.name);
     })
     .catch(function (error) {
       console.error(error);
@@ -25,7 +21,7 @@ function App() {
 
   useEffect(()=>{
     getData();
-  }, [])
+  }, [getData])
 
   const defName = (nome: string) => {
     setNome(nome);
@@ -34,8 +30,8 @@ function App() {
   return (
     <div>
       <strong>Ola {nome}</strong>
-
-      <strong>{advice}</strong>
+      <strong>Personagem: {characterName}</strong> 
+      <img src="../public/r2-d2.jpeg"/>
       <button onClick={() => defName('Tomate')}>Tomate</button>
       <button onClick={() => defName('Batata')}>Batata</button>
       <button onClick={() => defName('Beterraba')}>Beterraba</button>
